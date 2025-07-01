b6421bf5d834badc42ff604d9e809d9c9033b40c
Rhabi Mendoza <i_rhabiezra.mendoza@stratpoint.com>
2025-07-01 21:47:09 +0800

    added final react bit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import SplitText from "./effects/SplitText";
+import TrueFocus from "./effects/TrueFocus";
 import { Button } from "@/components/ui/button"
 import { IconDownload, IconBrandGithub, IconBrandLinkedin} from '@tabler/icons-react';
 
@@ -22,8 +23,8 @@ export default function Index() {
         <div className="flex flex-col">
 
     <SplitText
-        text="Hi, I am Rhabi Ezra"
-        className="text-6xl text-white font-bold"
+        text="Hi, I am Rhabi"
+        className="text-8xl text-white font-bold"
         delay={80}
         duration={0.3}
         ease="power3.out"
@@ -38,7 +39,19 @@ export default function Index() {
             <Button className="bg-red-400 me-3"><IconBrandGithub/> Github</Button>
             <Button className="bg-red-400 me-3"><IconBrandLinkedin/>LinkedIn</Button>
           </div>
+
+  <TrueFocus 
+    sentence="Runner Student Programmer"
+    manualMode={false}
+    blurAmount={5}
+    borderColor="white"
+    animationDuration={0.5}
+    pauseBetweenAnimations={1}
+  />
+
+
         </div>
+
       </div>
       {/* PENDING */}
 
