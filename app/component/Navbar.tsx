a4fb594ec2395c858b45be8770ddaece44024ca0
Rhabi Mendoza <i_rhabiezra.mendoza@stratpoint.com>
2025-07-01 13:44:11 +0800

    navbar done

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,47 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image'; 
+import {useState} from 'react';
+import {jua} from '../layout';
+
+export function Navbar(){
+
+    const [menuOpen,setMenuOpen] = useState(false);
+
+    return(
+
+        <nav className="fixed top-0 left-0 right-0 z-50">
+
+            <div className="flex flex-wrap items-center justify-between px-8 py-5">
+                
+                <Link href="/" className="flex items-center justify-center">
+                    <Image src="/images/icon.png" alt="logo" width={40} height={40} priority/>
+                    <span className={`text-4xl font-bold text-white px-3 ${jua.className}`}>REDPM</span>
+                </Link>
+
+                <button
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-800"
+                    aria-controls="navbar-default"
+                    aria-expanded={menuOpen}>
+                    <svg className="w-8 h-8" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+
+                <div className={'w-full md:block md:w-auto ' + (menuOpen ? 'block' : 'hidden')} id="navbar-default">
+                    <ul className={`text-2xl flex flex-col p-4 mt-4 items-center text-center md:p-0 md:flex-row md:space-x-8 md:mt-0 ${jua.className}`}>
+                        <li><Link href="/" className="block pt-2 text-white border-b-2 border-transparent hover:border-pink-600 transition-all duration-200">Home</Link></li>
+                        <li><Link href="/about" className="block pt-2 text-white border-b-2 border-transparent hover:border-pink-600 transition-all duration-200">About</Link></li>
+                        <li><Link href="/projects" className="block pt-2 text-white border-b-2 border-transparent hover:border-pink-600 transition-all duration-200">Projects</Link></li>
+                        <li><Link href="/contact" className="block pt-2 text-white border-b-2 border-transparent hover:border-pink-600 transition-all duration-200">Contact</Link></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </nav>
+
+    );
+
+}
